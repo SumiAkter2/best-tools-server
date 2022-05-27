2d342e0f91550dd314274d058113776c4e0f5cac
SumiAkter2 <akteralisha36@gmail.com>
2022-05-27 21:32:42 +0600

    added update profile,add product,all product,all

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { get } = require('express/lib/response');
-// let ObjectId = require('mongodb').ObjectID;
+
 app.use(express.json());
 app.use(cors());
 
@@ -17,38 +17,38 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-function sendAppointmentEmail(booking) {
-    const { patient, patientName, treatment, date, slot } = booking;
+// function sendAppointmentEmail(booking) {
+//     const { patient, patientName, treatment, date, slot } = booking;
 
-    var email = {
-        from: process.env.EMAIL_SENDER,
-        to: patient,
-        subject: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
-        text: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
-        html: `
-      <div>
-        <p> Hello ${patientName}, </p>
-        <h3>Your Appointment for ${treatment} is confirmed</h3>
-        <p>Looking forward to seeing you on ${date} at ${slot}.</p>
-        
-        <h3>Our Address</h3>
-        <p>Andor Killa Bandorban</p>
-        <p>Bangladesh</p>
-        <a href="https://web.programming-hero.com/">unsubscribe</a>
-      </div>
-    `
-    };
+//     var email = {
+//         from: process.env.EMAIL_SENDER,
+//         to: patient,
+//         subject: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+//         text: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+//         html: `
+//       <div>
+//         <p> Hello ${patientName}, </p>
+//         <h3>Your Appointment for ${treatment} is confirmed</h3>
+//         <p>Looking forward to seeing you on ${date} at ${slot}.</p>
 
-    emailClient.sendMail(email, function (err, info) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log('Message sent: ', info);
-        }
-    });
+//         <h3>Our Address</h3>
+//         <p>Andor Killa Bandorban</p>
+//         <p>Bangladesh</p>
+//         <a href="https://web.programming-hero.com/">unsubscribe</a>
+//       </div>
+//     `
+//     };
 
-}
+// emailClient.sendMail(email, function (err, info) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log('Message sent: ', info);
+//     }
+// });
+
+// }
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
@@ -68,7 +68,160 @@ async function run() {
         await client.connect();
         const collection = client.db("bestTools").collection("products");
         const userCollection = client.db("bestTools").collection("users");
-        const orderCollection = client.db("bestTools").collection("orders");
+        const reviewCollection = client.db("bestTools").collection("review")
+        // orderCollection = client.db("bestTools").collection("orders");
+
+
+        //delet:
+        app.delete("/deleteProducts/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await collection.deleteOne(query);
+            res.json(result);
+        });
+        ////reveiw:
+        app.post('/review', async (req, res) => {
+            const result = await reviewCollection.insertOne(req.body);
+            res.json(result);
+        })
+        //get review
+        app.get('/review', async (req, res) => {
+            const result = await reviewCollection.find({}).toArray();
+            res.json(result);
+        })
+        //////products:
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = collection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.get("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await collection.findOne(query);
+            res.send(item);
+        });
+        //update::
+        app.post("/update", async (req, res) => {
+            const result = await userCollection.insertOne(req.body);
+            res.json(result)
+        });
+        app.get('/update', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+        // user:
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+        })
+        // add product:
+        app.post("/addProduct", async (req, res) => {
+            const result = await collection.insertOne(req.body);
+            res.send(result)
+        });
+        ///make admin
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+
+        })
+
+        // delete   
+        app.delete("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await collection.deleteOne(query);
+            res.send(result);
+        });
+
+
+
+
+
+
+
+        //orders:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // const verifyAdmin = async (req, res, next) => {
         //     const requester = req.decoded.email;
@@ -219,13 +372,13 @@ async function run() {
         //         const result = await collection.deleteOne(query);
         //         res.send(result);
         //     });
-        //     app.get('/', (req, res) => {
-        //         res.send('Hello Best Tools co. !')
-        //     })
+        app.get('/', (req, res) => {
+            res.send('Hello Best Tools co. !')
+        })
 
-        //     app.listen(port, () => {
-        //         console.log(` Best Tools co. listening on port ${port}`)
-        //     })
+        app.listen(port, () => {
+            console.log(` Best Tools co. listening on port ${port}`)
+        })
     }
 
     finally {
