07a48181490dedba678eab0ef54da868efb900fe
SumiAkter2 <akteralisha36@gmail.com>
2022-05-28 09:48:20 +0600

    ass

--- index.js
+++ index.js
@@ -118,7 +118,8 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         })
-
+        ///hello
+        //comment
         app.get("/products/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
